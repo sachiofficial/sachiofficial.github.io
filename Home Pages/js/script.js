5e2757e8b2a35ee48a3882ec2845ecd2dc2605a1
sachiofficial <148868639+sachiofficial@users.noreply.github.com>
2024-05-31 22:24:24 +0530

    make chatbot

--- Home Pages/js/script.js
+++ Home Pages/js/script.js
@@ -1,4 +1,60 @@
+
+
+//step 1: get DOM
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 17000;
+
+nextDom.onclick = function(){
+    showSlider('next');    
+}
+
+prevDom.onclick = function(){
+    showSlider('prev');    
+}
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+function showSlider(type){
+    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+    
+    if(type === 'next'){
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+    }else{
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
+}
+
+
+
 'use strict';
+
+
 
 
 
@@ -111,3 +167,22 @@ window.addEventListener("resize", function () {
 
   moveSliderItem();
 });
+
+  window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('selection'); // Replace with your element selector
+  
+    for (const section of sections) {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const windowTop = window.pageYOffset;
+      const windowHeight = window.innerHeight;
+  
+      if (sectionTop >= windowTop && sectionTop + sectionHeight <= windowTop + windowHeight) {
+        section.classList.add('active');
+      } else {
+        section.classList.remove('active');
+      }
+    }
+  });
+  
+  
